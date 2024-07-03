d41fcc786b33794514d2aba334965fc28332e611
dandev947366 <dan.dev947366@gmail.com>
2024-07-03 10:42:29 +0300

    update db

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 #model
-class Todo(db.model):
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return f'<Task {self.id}>'
         
         
 @app.route('/')
